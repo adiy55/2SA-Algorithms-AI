201ab6a818dc15dbef4586688b48e8a95c0806a2
adiy55 <adiyafe55@gmail.com>
2021-11-14 22:18:11 +0200

    Add comments, remove unnecessary code

--- src/BayesBallAlgo.java
+++ src/BayesBallAlgo.java
@@ -1,6 +1,4 @@
 import java.util.HashMap;
-import java.util.LinkedList;
-import java.util.Queue;
 import java.util.Stack;
 
 public class BayesBallAlgo {
@@ -21,13 +19,13 @@ public class BayesBallAlgo {
                 return "no";
             }
             if (!v.isEvidence()) {
-                if (v.isFromChild()) {
+                if (v.isFromChild()) { // fromChild is true for the first time when v is the parent of an evidence node (can't go down)
                     for (int i = 0; i < v.getParents().size(); i++) {
                         VariableNode currParent = v.getParents().get(i);
                         currParent.setFromChild(true);
                         s.push(currParent);
                     }
-                } else { // can go to any child
+                } else { // v is the start node or not an evidence node (can only go down)
                     for (int i = 0; i < v.getChildren().size(); i++) {
                         VariableNode currChild = v.getChildren().get(i);
                         currChild.setFromParent(true);
@@ -46,78 +44,61 @@ public class BayesBallAlgo {
         return "yes";
     }
 
-    public String search2() {
-        Queue<VariableNode> q = new LinkedList<>();
-        q.add(data.get(input[0]));
-        while (!q.isEmpty()) {
-            VariableNode v = q.poll();
-            if (v.getName().equals(input[1])) {
-                return "no";
-            }
-            if (!v.isExplored()) {
-                v.setExplored(true);
-                if (!v.isEvidence()) {
-                    if (v.isFromChild()) {
-                        for (int i = 0; i < v.getParents().size(); i++) {
-                            VariableNode currParent = v.getParents().get(i);
-                            currParent.setFromChild(true);
-                            q.add(currParent);
-                        }
-                    }
-                    if (v.isFromChild() && !v.isFromParent()) { // can go to any child
-                        for (int i = 0; i < v.getChildren().size(); i++) {
-                            VariableNode currChild = v.getChildren().get(i);
-                            currChild.setFromParent(true);
-                            q.add(currChild);
-                        }
-                    }
-                }
-                if (v.isEvidence() && v.isFromParent() && !v.isFromChild()) {
-                    for (int i = 0; i < v.getParents().size(); i++) {
-                        VariableNode currParent = v.getParents().get(i);
-                        currParent.setFromChild(true);
-                        q.add(currParent);
-                    }
-                }
-            }
-        }
-        return "yes";
-    }
-
     public static void main(String[] args) {
         xpathParser xp = new xpathParser("alarm_net.xml");
         // B-E|
         // B-E|J=T
         String[] input = new String[]{"B", "E"};
-        VariableNode v = xp.getData().get("J");
-        v.setEvidence("T");
-
+        xp.getData().get("J").setEvidence("T");
 
         BayesBallAlgo bb = new BayesBallAlgo(xp.getData(), input);
-        System.out.println(bb.search());
-        System.out.println(bb.search2());
+//        System.out.println(bb.search());
+//        System.out.println(bb.search2());
 
         // ---------------------
 
         xpathParser xp2 = new xpathParser("network.xml");
-        String[] input2 = new String[]{"L", "B"};
-//        VariableNode v2 = (VariableNode) xp2.getData().get("T'");
-//        v2.setEvidence("T'");
-//        v2 = (VariableNode) xp2.getData().get("R");
-//        v2.setEvidence("T");
+        String[] input2 = new String[]{"B", "L"};
+        xp2.getData().get("T").setEvidence("T");
+        xp2.getData().get("R").setEvidence("T");
 
         BayesBallAlgo bb2 = new BayesBallAlgo(xp2.getData(), input2);
+//        System.out.println(bb2.search4());
         System.out.println(bb2.search());
-        System.out.println(bb2.search2());
     }
 
-//    private int findNodeIndex() { // returns first index found
-//        for (int i = 0; i < data.size(); i++) {
-//            if (data.get(i).getName().equals(input[0]) || data.get(i).getName().equals(input[1])) {
-//                return i;
+}
+
+//    private String search4() {
+//        Queue<VariableNode> s = new LinkedList<>();
+//        s.add(data.get(input[0]));
+//        while (!s.isEmpty()) {
+//            VariableNode v = s.poll();
+//            if (v.getName().equals(input[1])) {
+//                return "no";
+//            }
+//            if (!v.isEvidence()) {
+//                if (v.isFromChild()) { // fromChild is true when v is the parent of an evidence node (can't go down)
+//                    for (int i = 0; i < v.getParents().size(); i++) {
+//                        VariableNode currParent = v.getParents().get(i);
+//                        currParent.setFromChild(true);
+//                        s.add(currParent);
+//                    }
+//                } else { // v is the start node or not an evidence node (can only go down)
+//                    for (int i = 0; i < v.getChildren().size(); i++) {
+//                        VariableNode currChild = v.getChildren().get(i);
+//                        currChild.setFromParent(true);
+//                        s.add(currChild);
+//                    }
+//                }
+//            }
+//            if (v.isEvidence() && v.isFromParent()) {
+//                for (int i = 0; i < v.getParents().size(); i++) {
+//                    VariableNode currParent = v.getParents().get(i);
+//                    currParent.setFromChild(true);
+//                    s.add(currParent);
+//                }
 //            }
 //        }
-//        return -1;
+//        return "yes";
 //    }
-
-}
