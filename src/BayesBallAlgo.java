338181ff2fa5af8d62f449f523837a3c805177e1
adiy55 <adiyafe55@gmail.com>
2021-11-17 20:03:22 +0200

    Fix BayesBallAlgo bug

--- src/BayesBallAlgo.java
+++ src/BayesBallAlgo.java
@@ -10,38 +10,45 @@ public class BayesBallAlgo {
         this.input = input;
     }
 
-    private String search() { // DFS
+    public String search() { // DFS
         Stack<VariableNode> s = new Stack<>();
-        s.push(data.get(input[0]));
+        VariableNode v = data.get(input[0]);
+        s.push(v);
+        add_children(s, v);
+        add_parents(s, v);
         while (!s.isEmpty()) {
-            VariableNode v = s.pop();
+            v = s.pop();
             if (v.getName().equals(input[1])) {
                 return "no";
             }
             if (!v.isEvidence()) {
-                if (v.isFromChild()) { // fromChild is true for the first time when v is the parent of an evidence node (can't go down)
-                    for (int i = 0; i < v.getParents().size(); i++) {
-                        VariableNode currParent = v.getParents().get(i);
-                        currParent.setFromChild(true);
-                        s.push(currParent);
-                    }
-                } else { // v is the start node or not an evidence node (can only go down)
-                    for (int i = 0; i < v.getChildren().size(); i++) {
-                        VariableNode currChild = v.getChildren().get(i);
-                        currChild.setFromParent(true);
-                        s.push(currChild);
-                    }
+                if (v.isFromChild()) {
+                    add_parents(s, v);
+                } else {
+                    add_children(s, v);
                 }
             }
             if (v.isEvidence() && v.isFromParent()) {
-                for (int i = 0; i < v.getParents().size(); i++) {
-                    VariableNode currParent = v.getParents().get(i);
-                    currParent.setFromChild(true);
-                    s.push(currParent);
-                }
+                add_parents(s, v);
             }
         }
         return "yes";
+    }
+
+    private void add_parents(Stack<VariableNode> s, VariableNode v) {
+        for (int i = 0; i < v.getParents().size(); i++) {
+            VariableNode currParent = v.getParents().get(i);
+            currParent.setFromChild(true);
+            s.push(currParent);
+        }
+    }
+
+    private void add_children(Stack<VariableNode> s, VariableNode v) {
+        for (int i = 0; i < v.getChildren().size(); i++) {
+            VariableNode currChild = v.getChildren().get(i);
+            currChild.setFromParent(true);
+            s.push(currChild);
+        }
     }
 
     public static void main(String[] args) {
@@ -49,18 +56,20 @@ public class BayesBallAlgo {
         // B-E|
         // B-E|J=T
         String[] input = new String[]{"B", "E"};
-        xp.getData().get("J").setEvidence("T");
+//        xp.getData().get("J").setEvidence("T");
 
-        BayesBallAlgo bb = new BayesBallAlgo(xp.getData(), input);
+//        BayesBallAlgo bb = new BayesBallAlgo(xp.getData(), input);
 //        System.out.println(bb.search());
 //        System.out.println(bb.search2());
 
         // ---------------------
 
         xpathParser xp2 = new xpathParser("network.xml");
-        String[] input2 = new String[]{"B", "L"};
-        xp2.getData().get("T").setEvidence("T");
-        xp2.getData().get("R").setEvidence("T");
+        String[] input2 = new String[]{"L", "B"};
+        xp2.getData().get("D").setEvidence("T");
+//        xp2.getData().get("T").setEvidence("T");
+//        xp2.getData().get("R").setEvidence("T");
+
 
         BayesBallAlgo bb2 = new BayesBallAlgo(xp2.getData(), input2);
 //        System.out.println(bb2.search4());
