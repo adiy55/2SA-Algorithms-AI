9a13992693c558783829912f1ccbe3aca0fe5b62
adiy55 <adiyafe55@gmail.com>
2021-11-19 09:57:02 +0200

    Update Ex1

--- src/Ex1.java
+++ src/Ex1.java
@@ -1,21 +1,11 @@
-import java.io.FileNotFoundException;
-import java.util.HashMap;
-
 public class Ex1 {
 
-    public static void main(String[] args) throws FileNotFoundException {
 
-        String path = "C:\\Users\\adiya\\Documents\\Documents\\Uni\\2SA\\AI\\Project\\input.txt";
+    public static void main(String[] args) {
+        Network n = new Network("./input.txt");
+        System.out.println(n);
+        System.out.println(n.getResults());
 
-        Network N = new Network(path);
-        System.out.println(N.getReader().getBayesBallInput());
-        System.out.println(N.getReader().getVarEliminationInput());
-        System.out.println(N.getReader().getXml());
-        System.out.println();
-        System.out.println(N);
-
-        HashMap map = new HashMap();
-        map.values();
     }
 
 }
