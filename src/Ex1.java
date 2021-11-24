bde0634c6533c64bf4329259343cdbbffe0541c1
adiy55 <adiyafe55@gmail.com>
2021-11-25 01:30:00 +0200

    Update Ex1

--- src/Ex1.java
+++ src/Ex1.java
@@ -1,11 +1,34 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+
 public class Ex1 {
 
 
     public static void main(String[] args) {
-        Network n = new Network("./input.txt");
-        System.out.println(n);
-        System.out.println(n.getResults());
-
+//        Network n = new Network("./input.txt");
+//        System.out.println(n);
+//        System.out.println(n.getResults());
+        FileParser parser = new FileParser("./input3.txt");
+        HashMap<String, VariableNode> net = parser.getData();
+        ArrayList<String> queries = parser.getQueries();
+        ArrayList<String> results = new ArrayList<>();
+        for (String curr_query : queries) {
+            if (curr_query.contains("P(")) {
+                VariableEliminationAlgo vea = new VariableEliminationAlgo(net, curr_query);
+                String res = vea.RunAlgo();
+                results.add(res);
+                System.out.println(res);
+            } else {
+                BayesBallAlgo bba = new BayesBallAlgo(net, curr_query);
+                String res = bba.RunAlgo();
+                results.add(res);
+                System.out.println(res);
+            }
+            for (VariableNode v : net.values()) {
+                v.setEvidence(null);
+            }
+        }
+        System.out.println(results);
     }
 
 }
