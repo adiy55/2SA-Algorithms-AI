9b245d1384b66bc850a5ffaf035c76f6a8ec279e
adiy55 <adiyafe55@gmail.com>
2021-11-24 14:26:40 +0200

    Fix BayesBallAlgo bug

--- src/BayesBallAlgo.java
+++ src/BayesBallAlgo.java
@@ -44,21 +44,20 @@ public class BayesBallAlgo implements NetworkAlgo {
         Stack<VariableNode> s = new Stack<>();
         VariableNode v = data.get(query_nodes[0]);
         s.push(v);
+        v = s.pop();
         add_children(s, v);
         add_parents(s, v);
         while (!s.isEmpty()) {
-            v = s.pop();
             if (v.getName().equals(query_nodes[1])) {
                 return "no";
             }
+            v = s.pop();
             if (!v.isEvidence()) {
+                add_children(s, v);
                 if (v.isFromChild()) {
                     add_parents(s, v);
-                } else {
-                    add_children(s, v);
                 }
-            }
-            if (v.isEvidence() && v.isFromParent()) {
+            } else if (v.isEvidence() && v.isFromParent()) {
                 add_parents(s, v);
             }
         }
@@ -68,52 +67,21 @@ public class BayesBallAlgo implements NetworkAlgo {
     private void add_parents(Stack<VariableNode> s, VariableNode v) {
         for (int i = 0; i < v.getParents().size(); i++) {
             VariableNode currParent = v.getParents().get(i);
-            currParent.setFromChild(true);
-            s.push(currParent);
+            if (!currParent.isFromChild()) {
+                currParent.setFromChild(true);
+                s.push(currParent);
+            }
         }
     }
 
     private void add_children(Stack<VariableNode> s, VariableNode v) {
         for (int i = 0; i < v.getChildren().size(); i++) {
             VariableNode currChild = v.getChildren().get(i);
-            currChild.setFromParent(true);
-            s.push(currChild);
+            if (!currChild.isFromParent()) {
+                currChild.setFromParent(true);
+                s.push(currChild);
+            }
         }
     }
 
 }
-
-
-//    private String search4() {
-//        Queue<VariableNode> s = new LinkedList<>();
-//        s.add(data.get(input[0]));
-//        while (!s.isEmpty()) {
-//            VariableNode v = s.poll();
-//            if (v.getName().equals(input[1])) {
-//                return "no";
-//            }
-//            if (!v.isEvidence()) {
-//                if (v.isFromChild()) { // fromChild is true when v is the parent of an evidence node (can't go down)
-//                    for (int i = 0; i < v.getParents().size(); i++) {
-//                        VariableNode currParent = v.getParents().get(i);
-//                        currParent.setFromChild(true);
-//                        s.add(currParent);
-//                    }
-//                } else { // v is the start node or not an evidence node (can only go down)
-//                    for (int i = 0; i < v.getChildren().size(); i++) {
-//                        VariableNode currChild = v.getChildren().get(i);
-//                        currChild.setFromParent(true);
-//                        s.add(currChild);
-//                    }
-//                }
-//            }
-//            if (v.isEvidence() && v.isFromParent()) {
-//                for (int i = 0; i < v.getParents().size(); i++) {
-//                    VariableNode currParent = v.getParents().get(i);
-//                    currParent.setFromChild(true);
-//                    s.add(currParent);
-//                }
-//            }
-//        }
-//        return "yes";
-//    }
