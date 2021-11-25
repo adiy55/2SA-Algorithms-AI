f67ad637a229a14f4044dcb398aa0552e97bb4a3
adiy55 <adiyafe55@gmail.com>
2021-11-25 12:21:17 +0200

    Write output to file

--- src/Ex1.java
+++ src/Ex1.java
@@ -1,3 +1,6 @@
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
 
@@ -5,10 +8,7 @@ public class Ex1 {
 
 
     public static void main(String[] args) {
-//        Network n = new Network("./input.txt");
-//        System.out.println(n);
-//        System.out.println(n.getResults());
-        FileParser parser = new FileParser("./input3.txt");
+        FileParser parser = new FileParser("./input.txt");
         HashMap<String, VariableNode> net = parser.getData();
         ArrayList<String> queries = parser.getQueries();
         ArrayList<String> results = new ArrayList<>();
@@ -17,18 +17,32 @@ public class Ex1 {
                 VariableEliminationAlgo vea = new VariableEliminationAlgo(net, curr_query);
                 String res = vea.RunAlgo();
                 results.add(res);
-                System.out.println(res);
             } else {
                 BayesBallAlgo bba = new BayesBallAlgo(net, curr_query);
                 String res = bba.RunAlgo();
                 results.add(res);
-                System.out.println(res);
             }
             for (VariableNode v : net.values()) {
                 v.setEvidence(null);
             }
         }
         System.out.println(results);
+
+        try {
+            File output = new File("./output.txt");
+            FileWriter writer = new FileWriter(output);
+            for (int i = 0; i < results.size(); i++) {
+                if (i == results.size() - 1) {
+                    writer.write(results.get(i));
+                } else {
+                    writer.write(results.get(i) + System.lineSeparator());
+                }
+            }
+            writer.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
     }
 
 }
