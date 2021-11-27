3727076909c9c307f454fa948a0776b5c4fe2285
adiy55 <adiyafe55@gmail.com>
2021-11-27 10:43:19 +0200

    Add documentation

--- src/VariableNode.java
+++ src/VariableNode.java
@@ -3,26 +3,143 @@ import java.util.ArrayList;
 public class VariableNode {
 
     private String name; // variable name
-    private ArrayList<String> outcomes; // variable outcomes
-    private String evidence; // for query
-    private ArrayList<VariableNode> parents; // GIVEN
-    private ArrayList<VariableNode> children;
-    private ArrayList<Double> table;
+    private ArrayList<String> outcomes; // variable outcomes // T,F / v1,v2,v2 (according to xml)
+    private String evidence; // String that contains the given outcome, if unknown remains null
+    private ArrayList<VariableNode> parents; // GIVEN // list of parent nodes (for node, given == parent nodes)
+    private ArrayList<VariableNode> children; // list of children nodes (given == parent node of current child node)
+    private ArrayList<Double> table; // probabilities
     private boolean isFromChild;
     private boolean isFromParent;
     private CPT cpt;
     private boolean isCPTUsed;
 
+    /**
+     * VariableNode constructor.
+     * Creates a node for each variable parsed in the XML file.
+     * Other attributes are set once the definition is parsed, or during the algorithms.
+     *
+     * @param name     variable name
+     * @param outcomes variable outcomes
+     */
     public VariableNode(String name, ArrayList<String> outcomes) {
-        this.name = name; // node name
-        this.outcomes = outcomes; // T,F / v1,v2,v2 (according to xml)
-        evidence = null; // String that contains the given outcome, if unknown remains null
-        this.parents = new ArrayList<>(); // list of parent nodes (for node, given == parent nodes)
-        this.children = new ArrayList<>(); // list of children nodes (given == parent node of current child node)
-        this.table = null; // probabilities
+        this.name = name;
+        this.outcomes = outcomes;
+        evidence = null;
+        this.parents = new ArrayList<>();
+        this.children = new ArrayList<>();
+        this.table = null;
         isFromChild = isFromParent = false; // for BayesBall algorithm
         this.cpt = null; // constructor will be called when parsing definition / when initCPT method is called
         this.isCPTUsed = false; // to mark if cpt has been used for variable elimination
+    }
+
+    /**
+     * @return the name of the variable
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * @return ArrayList of the node's outcomes
+     */
+    public ArrayList<String> getOutcomes() {
+        return outcomes;
+    }
+
+    /**
+     * @return String outcome of the node, if the node is not evidence returns null
+     */
+    public String getEvidence() {
+        return evidence;
+    }
+
+    /**
+     * @param evidence outcome of the node in the current query
+     */
+    public void setEvidence(String evidence) {
+        this.evidence = evidence;
+    }
+
+    /**
+     * @return true if evidence is not null
+     */
+    public boolean isEvidence() {
+        return this.evidence != null;
+    }
+
+    /**
+     * Used to add a child node when a parent is being parsed (a node marked as FOR is added as a child if it has nodes
+     * marked as GIVEN in the XML file definition).
+     *
+     * @param vn pointer to child node
+     */
+    public void addChild(VariableNode vn) {
+        this.children.add(vn);
+    }
+
+    /**
+     * @return ArrayList of children nodes (pointers to them)
+     */
+    public ArrayList<VariableNode> getChildren() {
+        return children;
+    }
+
+    /**
+     * Attribute for the Bayes Ball algorithm.
+     *
+     * @return true if node was added from a child
+     */
+    public boolean isFromChild() {
+        return isFromChild;
+    }
+
+    /**
+     * @param fromChild true if a parent node was added from a child node
+     */
+    public void setFromChild(boolean fromChild) {
+        isFromChild = fromChild;
+    }
+
+    /**
+     * Used to set the parents (marked as GIVEN in the XML file) of the node when the definition is parsed.
+     *
+     * @param parents ArrayList containing pointers to the parent nodes
+     */
+    public void setParents(ArrayList<VariableNode> parents) {
+        this.parents = parents;
+    }
+
+    /**
+     * @return ArrayList of parent nodes (pointers to them)
+     */
+    public ArrayList<VariableNode> getParents() {
+        return parents;
+    }
+
+    /**
+     * Attribute for the Bayes Ball algorithm.
+     *
+     * @return true if node was added from a parent
+     */
+    public boolean isFromParent() {
+        return isFromParent;
+    }
+
+    /**
+     * @param fromParent true if a child node was added from a parent node
+     */
+    public void setFromParent(boolean fromParent) {
+        isFromParent = fromParent;
+    }
+
+    /**
+     * Used to set the probabilities of each node when the XML definitions are parsed.
+     *
+     * @param table ArrayList containing the probabilities of the node
+     */
+    public void setTable(ArrayList<Double> table) {
+        this.table = table;
     }
 
     /**
@@ -64,107 +181,6 @@ public class VariableNode {
     }
 
     /**
-     * @return String outcome of the node, if the node is not evidence returns null
-     */
-    public String getEvidence() {
-        return evidence;
-    }
-
-    /**
-     * @param evidence outcome of the node in the current query
-     */
-    public void setEvidence(String evidence) {
-        this.evidence = evidence;
-    }
-
-    /**
-     * @return true if evidence is not null
-     */
-    public boolean isEvidence() {
-        return this.evidence != null;
-    }
-
-    /**
-     * Attribute for the Bayes Ball algorithm.
-     *
-     * @return true if node was added from a child
-     */
-    public boolean isFromChild() {
-        return isFromChild;
-    }
-
-    /**
-     * Attribute for the Bayes Ball algorithm.
-     *
-     * @return true if node was added from a parent
-     */
-    public boolean isFromParent() {
-        return isFromParent;
-    }
-
-    /**
-     * @param fromChild true if a parent node was added from a child node
-     */
-    public void setFromChild(boolean fromChild) {
-        isFromChild = fromChild;
-    }
-
-    /**
-     * @param fromParent true if a child node was added from a parent node
-     */
-    public void setFromParent(boolean fromParent) {
-        isFromParent = fromParent;
-    }
-
-    /**
-     * Used to set the probabilities of each node when the XML definitions are parsed.
-     *
-     * @param table ArrayList containing the probabilities of the node
-     */
-    public void setTable(ArrayList<Double> table) {
-        this.table = table;
-    }
-
-    /**
-     * Used to set the parents (marked as GIVEN in the XML file) of the node when the definition is parsed.
-     *
-     * @param parents ArrayList containing pointers to the parent nodes
-     */
-    public void setParents(ArrayList<VariableNode> parents) {
-        this.parents = parents;
-    }
-
-    /**
-     * Used to add a child node when a parent is being parsed (a node marked as FOR is added as a child if it has nodes
-     * marked as GIVEN in the XML file definition).
-     *
-     * @param vn pointer to child node
-     */
-    public void addChild(VariableNode vn) {
-        this.children.add(vn);
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public ArrayList<VariableNode> getParents() {
-        return parents;
-    }
-
-    public ArrayList<VariableNode> getChildren() {
-        return children;
-    }
-
-    public ArrayList<Double> getTable() {
-        return table;
-    }
-
-    public ArrayList<String> getOutcomes() {
-        return outcomes;
-    }
-
-    /**
      * @return string containing the node's information parsed from the XML
      */
     public String toString() {
@@ -181,4 +197,5 @@ public class VariableNode {
         s += ", TABLE:" + table + "}";
         return s;
     }
+
 }
