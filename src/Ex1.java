97e2edb9b03b4adff5c9c92b8c7ae27634131619
adiy55 <adiyafe55@gmail.com>
2021-11-29 17:01:43 +0200

    Wrap the entire function with try and catch

--- src/Ex1.java
+++ src/Ex1.java
@@ -5,31 +5,31 @@ import java.util.ArrayList;
 import java.util.HashMap;
 
 public class Ex1 {
+    // todo: ./input??? check private and public functions
 
+    // todo: add try and catch around parse function and returned query string (return space/empty string???)
 
     public static void main(String[] args) {
-        FileParser parser = new FileParser("./input.txt");
-        HashMap<String, VariableNode> net = parser.getData();
-        ArrayList<String> queries = parser.getQueries();
-        ArrayList<String> results = new ArrayList<>();
-        for (String curr_query : queries) {
-            if (curr_query.contains("P(")) {
-                VariableEliminationAlgo vea = new VariableEliminationAlgo(net, curr_query);
-                String res = vea.RunAlgo();
-                results.add(res);
-            } else {
-                BayesBallAlgo bba = new BayesBallAlgo(net, curr_query);
-                String res = bba.RunAlgo();
-                results.add(res);
-            }
-            for (VariableNode v : net.values()) {
-                v.setEvidence(null);
-            }
-        }
-        System.out.println(results);
-
         try {
-            File output = new File("./output.txt");
+            FileParser parser = new FileParser("input.txt");
+            HashMap<String, VariableNode> net = parser.getData();
+            ArrayList<String> queries = parser.getQueries();
+            ArrayList<String> results = new ArrayList<>();
+            for (String curr_query : queries) {
+                if (curr_query.contains("P(")) {
+                    VariableEliminationAlgo vea = new VariableEliminationAlgo(net, curr_query);
+                    String res = vea.RunAlgo();
+                    results.add(res);
+                } else {
+                    BayesBallAlgo bba = new BayesBallAlgo(net, curr_query);
+                    String res = bba.RunAlgo();
+                    results.add(res);
+                }
+                for (VariableNode v : net.values()) {
+                    v.setEvidence(null);
+                }
+            }
+            File output = new File("output.txt");
             FileWriter writer = new FileWriter(output);
             for (int i = 0; i < results.size(); i++) {
                 if (i == results.size() - 1) {
@@ -45,4 +45,6 @@ public class Ex1 {
 
     }
 
+
+    // todo: test functions (input parsers), documentation, catch invalid input
 }
